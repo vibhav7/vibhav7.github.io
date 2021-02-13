edcf2459551dc12194268edd362f85f72d278e5a
vibhav7 <vibhav.kumar@growisto.com>
2021-02-14 00:10:17 +0530

    added eye

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -2,9 +2,29 @@ import React from 'react';
 import Wrapper from '../components/core/Wrapper';
 
 function ContactUs(props) {
+  function eyeMovementhandler() {
+    // $(".move-area").mousemove(function (event) {
+    // var eye = $(".eye");
+    // var x = (eye.offset().left) + (eye.width() / 2);
+    // var y = (eye.offset().top) + (eye.height() / 2);
+    // var rad = Math.atan2(event.pageX - x, event.pageY - y);
+    // var rot = (rad * (180 / Math.PI) * -1) + 180;
+    // eye.css({
+    //   '-webkit-transform': 'rotate(' + rot + 'deg)',
+    //   '-moz-transform': 'rotate(' + rot + 'deg)',
+    //   '-ms-transform': 'rotate(' + rot + 'deg)',
+    //   'transform': 'rotate(' + rot + 'deg)'
+    // });
+    // });
+  }
   return (
     <Wrapper>
-      This is ContactUs
+      <section onMouseMove={eyeMovementhandler} className="move-area">
+        <div className='eye-container'>
+          <div className='eye'></div>
+          <div className='eye'></div>
+        </div>
+      </section>
     </Wrapper>
   );
 }
