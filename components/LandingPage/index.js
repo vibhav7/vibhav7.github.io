da32e7d8db7211a6bdf6086bbf3477a914beb01a
vibhav7 <vibhav.kumar@growisto.com>
2021-02-14 00:50:47 +0530

    added trail comp

--- components/LandingPage/index.js
+++ components/LandingPage/index.js
@@ -1,9 +1,11 @@
 import styles from "../../styles/Home.module.css"
+import Trail from "../core/Trail"
 
 export default function LandingPage() {
   return (
     <div className="">
       <div className={`${styles.HeroContainerSvg} w-full`}>
+        {/* <Trail /> */}
         <img className="mx-auto" src="/static/svg/undraw_Nature_fun_re_iney.svg" />
       </div>
     </div>
