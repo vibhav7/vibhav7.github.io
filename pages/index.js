3242994b622f51f1e77e9126db30cbb900cc5bea
vibhav7 <vibhav.kumar@growisto.com>
2021-02-13 19:57:19 +0530

    added nextjs repo

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Vibhav's Portfolio</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+     Vibhav
+    </div>
+  )
+}
