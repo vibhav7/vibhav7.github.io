b6d040a752e927229e62e965b9c4c192f5a09da7
vibhav7 <vibhav.kumar@growisto.com>
2021-02-21 02:05:35 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Head from 'next/head'
 import Wrapper from '../components/core/Wrapper'
 import LandingPage from '../components/LandingPage'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
