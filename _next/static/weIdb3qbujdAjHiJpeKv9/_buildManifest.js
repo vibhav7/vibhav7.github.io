942746ab2c13b755018329459e07561e0fa62205
vibhav7 <vibhav7@users.noreply.github.com>
2021-04-10 19:04:30 +0000

    Deploying to gh-pages from @ vibhav7/vibhav7.github.io@b8dbf37af6ed3cda558fa6b2b5e776e321d2c2cf 🚀

--- _next/static/weIdb3qbujdAjHiJpeKv9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F7f1337213502f2d66ab5.css","static\u002Fchunks\u002Fpages\u002Findex-51a417748974eabf8221.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6562f6bbb7cee4c25344.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
