bc1304c6c0a205ec059da6922097a10fabf420d9
vibhav7 <vibhav.kumar@growisto.com>
2021-04-11 00:23:34 +0530

    change-bg

--- components/LandingPage/index.js
+++ components/LandingPage/index.js
@@ -4,8 +4,8 @@ import styles from "./index.module.scss"
 export default function LandingPage() {
 
   return (
-    <div className="">
-      <div className="min-h-screen text-center">Working on</div>
+    <div className=''>
+      <div className="text-center ">Working on</div>
     </div >
   )
 }
